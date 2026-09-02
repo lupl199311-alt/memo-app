f1fc8d30f6def440105570e0ac739560569ac20e
lupl199311-alt <lupl199311@gmail.com>
2026-09-02 23:55:13 +0900

    Add instant todo entry and move sync status

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "luplmemo-v3";
+const CACHE_NAME = "luplmemo-v4";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
