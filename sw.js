f0771e9acd10c8d147334f446c7140552a373f18
lupl199311-alt <leehuieun@gmail.com>
2026-09-09 19:53:51 +0900

    Refresh control layout cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "luplmemo-v6";
+const CACHE_NAME = "luplmemo-v7";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
